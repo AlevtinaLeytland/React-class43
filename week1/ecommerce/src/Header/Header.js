7680f32d72fa5ae467878c8873e82c7dfed0aa84
AlevtinaLeytland <clavusclavo321@gmail.com>
2023-08-08 12:57:00 +0200

    Last edit

--- week1/ecommerce/src/Header/Header.js
+++ week1/ecommerce/src/Header/Header.js
@@ -1,37 +1,34 @@
-import './Header.css'
-import categories from '../fake-data/all-categories'
-import { useEffect, useState } from 'react'
+import "./Header.css";
+import categories from "../fake-data/all-categories";
+import { useState } from "react";
 
 function Header(props) {
-	const [selectedCategory, setSelectedCategory] = useState('')
+  const [selectedCategory, setSelectedCategory] = useState("");
 
-	const handleClick = (category) => {
-		setSelectedCategory(selectedCategory === category ? '' : category)
-	}
+  const handleClick = (category) => {
+    props.filter(selectedCategory === category ? "" : category);
+    setSelectedCategory(selectedCategory === category ? "" : category);
+  };
 
-	useEffect(() => {
-		props.filter(selectedCategory)
-	}, [props, selectedCategory])
-
-	return (
-		<div className='header'>
-			<h1>Products</h1>
-			<nav className='nav-menu'>
-				<ul className='categories-list'>
-					{categories.map((category) => (
-						<li
-							key={category}
-							onClick={() => handleClick(category)}
-							className={`category ${
-								selectedCategory === category && 'active'
-							}`}
-						>
-							{category}
-						</li>
-					))}
-				</ul>
-			</nav>
-		</div>
-	)
+  return (
+    <div className="header">
+      <h1>Products</h1>
+      <nav className="nav-menu">
+        <ul className="categories-list">
+          {categories.map((category) => (
+            <li
+              key={category}
+              onClick={() => handleClick(category)}
+              className={`category ${
+                selectedCategory === category && "active"
+              }`}
+            >
+              {category}
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </div>
+  );
 }
-export default Header
+export default Header;
